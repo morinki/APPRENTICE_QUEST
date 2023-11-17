1cd33c778ccba858ef1c8ccca3ef0c2edb3046d0
morinki <rinki29171@gmail.com>
2023-11-17 15:13:53 +0900

    最初の分岐処理

--- password_manager.sh
+++ password_manager.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 echo "パスワードマネージャーへようこそ！"
+read -p "次の選択肢から入力してください(Add Password/Get Password/Exit):" choise
 
-read -p "サービス名を入力してください：" service_name
-read -p "ユーザー名を入力してください：" user_name
-read -s -p "パスワードを入力してください：" password
-echo
+case $choise in
+  "Add Password")
+    read -p "サービス名を入力してください：" service_name
+    read -p "ユーザー名を入力してください：" user_name
+    read -s -p "パスワードを入力してください：" password
+    echo  
+    echo "$service_name:$user_name:$password" >> password_manager.txt
+    echo "パスワードの追加は成功しました"
+    ;;
 
-echo "$service_name:$user_name:$password" >> password_manager.txt
+  "Get Password")
+    echo "入力確認、$choise"
+    ;;
 
-echo "Thank you❗️"
+  "Exit")  
+    echo "Thank you❗️"
+    ;;
+
+  *)
+    echo "入力が間違えています。 Add Password/Get Password/Exit から入力してください。"
+    ;;
+
+esac
