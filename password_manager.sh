fdab9ad7cc082cd3cd9e53f2aacf46de1216b919
morinki <rinki29171@gmail.com>
2023-11-17 16:02:53 +0900

    繰り返し処理の追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 echo "パスワードマネージャーへようこそ！"
-read -p "次の選択肢から入力してください(Add Password/Get Password/Exit):" choise
+while true; do
+  read -p "次の選択肢から入力してください(Add Password/Get Password/Exit):" choise
 
-case $choise in
-  "Add Password")
-    read -p "サービス名を入力してください：" service_name
-    read -p "ユーザー名を入力してください：" user_name
-    read -s -p "パスワードを入力してください：" password
-    echo  
-    echo "$service_name:$user_name:$password" >> password_manager.txt
-    echo "パスワードの追加は成功しました"
-    ;;
+  case $choise in
+    "Add Password")
+      read -p "サービス名を入力してください：" service_name
+      read -p "ユーザー名を入力してください：" user_name
+      read -s -p "パスワードを入力してください：" password
+      echo  
+      echo "$service_name:$user_name:$password" >> password_manager.txt
+      echo "パスワードの追加は成功しました"
+      ;;
 
-  "Get Password")
-    echo "入力確認、$choise"
-    ;;
+    "Get Password")
+      echo "入力確認、$choise"
+      ;;
 
-  "Exit")  
-    echo "Thank you❗️"
-    ;;
+    "Exit")  
+      echo "Thank you❗️"
+      break
+      ;;
 
-  *)
-    echo "入力が間違えています。 Add Password/Get Password/Exit から入力してください。"
-    ;;
+    *)
+      echo "入力が間違えています。 Add Password/Get Password/Exit から入力してください。"
+      ;;
 
-esac
+  esac
+done
